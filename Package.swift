067809f274e512130d4c16adf0a71eaefd0fc485
jamesarmer <james@secureprivacy.ai>
2025-06-13 09:33:03 +0100

    chore: add checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "SPMobileConsent",
             url: "https://cdn.secureprivacy.ai/cocoapods/SPMobileConsent.xcframework-v0.2.3-beta.zip",
-            checksum: "your_computed_checksum_here"
+            checksum: "5c1601281e2803d1d58e73e8d084140c487cffc417d245f20ca311056f87fb34"
         )
     ]
 )
