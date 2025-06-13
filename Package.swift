e83a026ea75729da9524b45dd42c926544ba216a
jamesarmer <james@secureprivacy.ai>
2025-06-13 09:41:06 +0100

    fix: correct target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "SecurePrivacyMobileConsent",
-            targets: ["SecurePrivacyMobileConsent"]
+            targets: ["SPMobileConsent"]
         ),
     ],
     targets: [
