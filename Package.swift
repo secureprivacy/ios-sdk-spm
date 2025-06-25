794f5851cc2c846648c3752163f3bb642312b160
jamesarmer <james@secureprivacy.ai>
2025-06-25 10:27:25 +0100

    release: 0.2.4-beta

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SPMobileConsent",
-            url: "https://cdn.secureprivacy.ai/cocoapods/SPMobileConsent.xcframework-v0.2.3-beta.zip",
-            checksum: "5c1601281e2803d1d58e73e8d084140c487cffc417d245f20ca311056f87fb34"
+            url: "https://cdn.secureprivacy.ai/cocoapods/SPMobileConsent.xcframework-v0.2.4-beta.zip",
+            checksum: "fe2d1a6a85e54f7a49566c5925791260c79a6da4fcb5197a6887787a04675725"
         )
     ]
 )
